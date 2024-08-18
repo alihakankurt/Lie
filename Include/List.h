463d8e26e5695352b5eafe9c9a6a75a1de3d5549
alihakankurt <kurtalihakan@outlook.com>
2024-08-18 21:33:26 +0300

    Update `List` template to use `MemoryCopy` function in remove and insert operations

--- Include/List.h
+++ Include/List.h
@@ -19,73 +19,79 @@
     bool InsertTo##Name(Name* list, Type value, usize insertIndex); \
     bool RemoveFrom##Name(Name* list, usize removeIndex);
 
-#define ImplementList(Name, Type)                                            \
-    void Initialize##Name(Name* list)                                        \
-    {                                                                        \
-        list->Count = 0;                                                     \
-        list->Capacity = 0;                                                  \
-        list->Values = NULL;                                                 \
-    }                                                                        \
-                                                                             \
-    void Finalize##Name(Name* list)                                          \
-    {                                                                        \
-        MemoryFree(list->Values);                                            \
-    }                                                                        \
-                                                                             \
-    void Extend##Name(Name* list)                                            \
-    {                                                                        \
-        usize capacity = (list->Capacity == 0) ? 2 : list->Capacity * 2;     \
-        Type* values = (Type*)MemoryAllocate(capacity * sizeof(Type));       \
-        if (list->Values != NULL)                                            \
-        {                                                                    \
-            MemoryCopy(values, list->Values, list->Count * sizeof(Type));    \
-            MemoryFree(list->Values);                                        \
-        }                                                                    \
-                                                                             \
-        list->Values = values;                                               \
-        list->Capacity = capacity;                                           \
-    }                                                                        \
-                                                                             \
-    void Clear##Name(Name* list)                                             \
-    {                                                                        \
-        list->Count = 0;                                                     \
-    }                                                                        \
-                                                                             \
-    void AddTo##Name(Name* list, Type value)                                 \
-    {                                                                        \
-        if (list->Count == list->Capacity)                                   \
-            Extend##Name(list);                                              \
-                                                                             \
-        list->Values[list->Count] = value;                                   \
-        list->Count += 1;                                                    \
-    }                                                                        \
-                                                                             \
-    bool InsertTo##Name(Name* list, Type value, usize insertIndex)           \
-    {                                                                        \
-        if (insertIndex > list->Count)                                       \
-            return false;                                                    \
-                                                                             \
-        if (list->Count == list->Capacity)                                   \
-            Extend##Name(list);                                              \
-                                                                             \
-        for (usize index = list->Count; index > insertIndex; index -= 1)     \
-            list->Values[index] = list->Values[index - 1];                   \
-                                                                             \
-        list->Values[insertIndex] = value;                                   \
-        list->Count += 1;                                                    \
-        return true;                                                         \
-    }                                                                        \
-                                                                             \
-    bool RemoveFrom##Name(Name* list, usize removeIndex)                     \
-    {                                                                        \
-        if (removeIndex >= list->Count)                                      \
-            return false;                                                    \
-                                                                             \
-        for (usize index = removeIndex; index < list->Count - 1; index += 1) \
-            list->Values[index] = list->Values[index + 1];                   \
-                                                                             \
-        list->Count -= 1;                                                    \
-        return true;                                                         \
+#define ImplementList(Name, Type)                                         \
+    void Initialize##Name(Name* list)                                     \
+    {                                                                     \
+        list->Count = 0;                                                  \
+        list->Capacity = 0;                                               \
+        list->Values = NULL;                                              \
+    }                                                                     \
+                                                                          \
+    void Finalize##Name(Name* list)                                       \
+    {                                                                     \
+        MemoryFree(list->Values);                                         \
+    }                                                                     \
+                                                                          \
+    void Extend##Name(Name* list)                                         \
+    {                                                                     \
+        usize capacity = (list->Capacity == 0) ? 2 : list->Capacity * 2;  \
+        Type* values = (Type*)MemoryAllocate(capacity * sizeof(Type));    \
+        if (list->Values != NULL)                                         \
+        {                                                                 \
+            MemoryCopy(values, list->Values, list->Count * sizeof(Type)); \
+            MemoryFree(list->Values);                                     \
+        }                                                                 \
+                                                                          \
+        list->Values = values;                                            \
+        list->Capacity = capacity;                                        \
+    }                                                                     \
+                                                                          \
+    void Clear##Name(Name* list)                                          \
+    {                                                                     \
+        list->Count = 0;                                                  \
+    }                                                                     \
+                                                                          \
+    void AddTo##Name(Name* list, Type value)                              \
+    {                                                                     \
+        if (list->Count == list->Capacity)                                \
+            Extend##Name(list);                                           \
+                                                                          \
+        list->Values[list->Count] = value;                                \
+        list->Count += 1;                                                 \
+    }                                                                     \
+                                                                          \
+    bool InsertTo##Name(Name* list, Type value, usize insertIndex)        \
+    {                                                                     \
+        if (insertIndex > list->Count)                                    \
+            return false;                                                 \
+                                                                          \
+        if (list->Count == list->Capacity)                                \
+            Extend##Name(list);                                           \
+                                                                          \
+        MemoryCopy(                                                       \
+            list->Values + insertIndex + 1,                               \
+            list->Values + insertIndex,                                   \
+            (list->Count - insertIndex) * sizeof(Type)                    \
+        );                                                                \
+                                                                          \
+        list->Values[insertIndex] = value;                                \
+        list->Count += 1;                                                 \
+        return true;                                                      \
+    }                                                                     \
+                                                                          \
+    bool RemoveFrom##Name(Name* list, usize removeIndex)                  \
+    {                                                                     \
+        if (removeIndex >= list->Count)                                   \
+            return false;                                                 \
+                                                                          \
+        MemoryCopy(                                                       \
+            list->Values + removeIndex,                                   \
+            list->Values + removeIndex + 1,                               \
+            (list->Count - removeIndex - 1) * sizeof(Type)                \
+        );                                                                \
+                                                                          \
+        list->Count -= 1;                                                 \
+        return true;                                                      \
     }
 
 #endif
